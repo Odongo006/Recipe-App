57aee1f09d30a81070e21a6304721d15eef99a90
rails --v <ruby -v>
2023-07-12 22:04:31 +0300

    Add model files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  resources :food_recipes
-  resources :recipes
-  resources :users
+  # devise_for :users
+  
+  resources :recipes, except: [:edit] do
+  resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
+  end
+  
   resources :foods
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
+  
+  # Add the route for the edit action
+  get 'recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  get '/general_shopping_list' , to: 'users#shopping_list'
+  get 'recipes/:recipe_id/new_ingredient' , to: 'recipes#new_ingredient' , as: :new_ingredient
+  post 'recipes/:recipe_id/new_ingredient' , to: 'recipes#create_ingredient' , as: :create_ingredient
+  root "foods#index"
 end
