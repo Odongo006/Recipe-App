495005eea8b60efd33e931ef6c752811a5858432
rails --v <ruby -v>
2023-07-14 18:47:49 +0300

    Update controller

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,18 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy
+
+  validates :name, presence: true
+  validates :preparation_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
+  validates :cooking_time, numericality: { only_integer: true, greater_or_equal_to: 0 }
+  validates :description, presence: true
+  validates :public, inclusion: { in: [true, false], message: 'should be a boolean value' }
+
+  def all_food_items
+    recipe_foods.select(:food_id).distinct.count
+  end
+
+  def total_price
+    recipe_foods.includes(:food).sum { |recipe_food| recipe_food.food.price * recipe_food.quantity }
+  end
 end
